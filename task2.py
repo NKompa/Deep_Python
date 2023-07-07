5ec3bbb8a946ab357295e88fdc1de9579a38124e
NKompa <nkompa7@gmail.com>
2023-07-07 12:29:53 +0300

    Get dict of bonus.

--- task2.py
+++ task2.py
@@ -1,6 +1,6 @@
-names = ["Misha", "Grisha", "Masha"]
-rates = [100000, 150000, 130000]
-bonus_percent = ["10.25%", "7.5%", "8.75%"]
+names = ["Misha", "Grisha", "Masha", "Dasha"]
+rates = [100000, 150000, 130000, 160000]
+bonus_percent = ["10.25%", "7.5%", "8.75%", "9%"]
 
 result = {name: rate * float(bonus[:-1])/100 for name, rate, bonus in zip(names, rates, bonus_percent)}
 
